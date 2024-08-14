843560bf086807fe1727db7c8c916708d18a9699
Kirill Bravin <kshaadoo1991@gmail.com>
2024-08-14 15:21:34 +0300

    Added users page

---
+++ Postie.FrontEnd/Pages/Users.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Postie.FrontEnd.Pages
+{
+    public class UsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
