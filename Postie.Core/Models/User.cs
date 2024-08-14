a05b9fdf6634e742b0c16fa422e0e81ba4487180
Edgar Sokol <edgar.sokol@gmail.com>
2024-08-14 11:57:38 +0300

    small change

--- Postie.Core/Models/User.cs
+++ Postie.Core/Models/User.cs
@@ -5,12 +5,17 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public User() { }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
         public User(int id, string name)
         {
             Id = id;
             Name = name;
         }
-
-        public User() { }
     }
 }
