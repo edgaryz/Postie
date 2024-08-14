8eececaf432b85873686fb1e31f50a6fd7bfc9b7
Kirill Bravin <kshaadoo1991@gmail.com>
2024-08-14 11:37:59 +0300

    User change

--- Postie.Core/Models/User.cs
+++ Postie.Core/Models/User.cs
@@ -4,6 +4,13 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
+
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public User() { }
     }
 }
